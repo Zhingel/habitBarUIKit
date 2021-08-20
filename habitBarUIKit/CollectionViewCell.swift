45197085e246615637da6177c2618bf453c50db4
Stas <staszhingel@icloud.com>
2021-08-20 22:00:27 +0300

    collectionView

---
+++ habitBarUIKit/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  habitBarUIKit
+//
+//  Created by Стас Жингель on 20.08.2021.
+//
+
+import UIKit
+
+class cell: UICollectionViewCell {
+    
+    @IBOutlet weak var title: UILabel!
+    
+}
--- habitBarUIKit/ViewController.swift
+++ habitBarUIKit/ViewController.swift
@@ -2,18 +2,56 @@
 //  ViewController.swift
 //  habitBarUIKit
 //
-//  Created by Стас Жингель on 18.08.2021.
+//  Created by Стас Жингель on 19.08.2021.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    let itemsPerRow: CGFloat = 1
+    let sectionInserts = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+    }
+    
+
+   
+    
+}
+extension ViewController : UICollectionViewDelegateFlowLayout, UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! cell
+        cell.backgroundColor = .yellow
+        cell.layer.cornerRadius = 20.0
+        cell.title.text = "1111111111111"
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let paddingWidth = sectionInserts.left * (itemsPerRow + 1)
+        let availibleWidth = collectionView.frame.width - paddingWidth
+        let widthPerItem = availibleWidth/itemsPerRow
+        return CGSize(width: widthPerItem, height: 140)
     }
 
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return sectionInserts
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return sectionInserts.left
+    }
 
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return sectionInserts.left
+    }
+    
 }
-
